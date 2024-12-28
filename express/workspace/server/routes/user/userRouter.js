4bc25f124997904340358bc988140f1b1bdeb6d8
whitepanguin <dun04045@gmail.com>
2024-12-28 20:23:57 +0900

    Add: payment

--- express/workspace/server/routes/user/userRouter.js
+++ express/workspace/server/routes/user/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, modify, remove } from '../../controller/user/userController.js';
+import { register, login, modify, remove, updatePicture } from '../../controller/user/userController.js';
 
 const userRouter = express.Router()
 
@@ -7,5 +7,6 @@ userRouter.post("/register", register)
 userRouter.post("/login", login)
 userRouter.put("/modify", modify)
 userRouter.delete("/remove", remove)
+userRouter.post("/picture", updatePicture)
 
 export default userRouter;
